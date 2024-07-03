aa54dbc98fd09a7241d9abbd9e1cacf42a2975cd
jnj256 <jnj256@gmail.com>
2024-07-03 13:37:01 +0100

    v10_FactorizePhys; LR=1e-3, Mul-NoRes, RGB:  R=1, S=1, ST=4, Thermal: R=4, S=5, ST=5,  NMF

--- neural_methods/model/FactorizePhys.py
+++ neural_methods/model/FactorizePhys.py
@@ -15,9 +15,9 @@ nf = [8, 16, 16, 16]
 model_config = {
     "MD_FSAM": True,
     "MD_TYPE": "NMF",
-    "MD_R": 4,
-    "MD_S": 5,
-    "MD_STEPS": 6,
+    "MD_R": 1,
+    "MD_S": 1,
+    "MD_STEPS": 4,
     "INV_T": 1,
     "ETA": 0.9,
     "RAND_INIT": True,
@@ -871,14 +871,14 @@ class BVP_Head(nn.Module):
             # # Residual connection: 
             # factorized_embeddings = voxel_embeddings + self.fsam_norm(att_mask)
 
-            # # Multiplication
-            # x = torch.mul(voxel_embeddings - voxel_embeddings.min() + self.bias1, att_mask - att_mask.min() + self.bias1)
-            # factorized_embeddings = self.fsam_norm(x)
-
-            # Multiplication with Residual connection
+            # Multiplication
             x = torch.mul(voxel_embeddings - voxel_embeddings.min() + self.bias1, att_mask - att_mask.min() + self.bias1)
             factorized_embeddings = self.fsam_norm(x)
-            factorized_embeddings = voxel_embeddings + factorized_embeddings
+
+            # # Multiplication with Residual connection
+            # x = torch.mul(voxel_embeddings - voxel_embeddings.min() + self.bias1, att_mask - att_mask.min() + self.bias1)
+            # factorized_embeddings = self.fsam_norm(x)
+            # factorized_embeddings = voxel_embeddings + factorized_embeddings
             
             # # Concatenate
             # factorized_embeddings = torch.cat([voxel_embeddings, self.fsam_norm(x)], dim=1)
@@ -931,7 +931,7 @@ class FactorizePhys(nn.Module):
         if self.debug:
             print("nf:", nf)
 
-        self.voxel_embeddings = encoder_block(self.in_channels, dropout_rate=dropout, debug=debug)
+        self.encoder = encoder_block(self.in_channels, dropout_rate=dropout, debug=debug)
 
         self.rppg_head = BVP_Head(md_config, device=device, dropout_rate=dropout, debug=debug)
 
@@ -974,7 +974,7 @@ class FactorizePhys(nn.Module):
         if self.debug:
             print("Diff Normalized shape", x.shape)
 
-        voxel_embeddings = self.voxel_embeddings(x)
+        voxel_embeddings = self.encoder(x)
         if self.debug:
             print("voxel_embeddings.shape", voxel_embeddings.shape)
         
