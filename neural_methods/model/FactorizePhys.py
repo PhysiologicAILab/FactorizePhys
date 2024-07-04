9c5c0b4f6ae06cbdfa303722e72602233ea50bdc
jnj256 <jnj256@gmail.com>
2024-07-04 11:53:04 +0100

    FactorizePhys; LR=1e-3, Mul-NoRes, Multiple Configs; NMF only during training

--- neural_methods/model/FactorizePhys.py
+++ neural_methods/model/FactorizePhys.py
@@ -818,13 +818,13 @@ class BVP_Head(nn.Module):
             inC = nf[3]
 
         self.conv_decoder = nn.Sequential(
-            nn.Conv3d(inC, nf[0], (3, 3, 3), stride=(1, 2, 2), padding=(1, 0, 0)),
+            nn.Conv3d(inC, nf[0], (3, 5, 5), stride=(1, 1, 1), padding=(1, 0, 0)),
             nn.Tanh(),
             nn.InstanceNorm3d(nf[0]),
 
             nn.Dropout3d(p=dropout_rate),
 
-            nn.Conv3d(nf[0], 1, (3, 3, 3), stride=(1, 1, 1), padding=(1, 0, 0)),
+            nn.Conv3d(nf[0], 1, (5, 3, 3), stride=(1, 1, 1), padding=(2, 0, 0)),
         )
 
 
