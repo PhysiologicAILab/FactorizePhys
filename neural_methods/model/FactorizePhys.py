1cb6b520ef833e58f695a1c78f4ec9ea96cdeb6c
jnj256 <jnj256@gmail.com>
2024-07-05 16:58:02 +0100

    FactorizePhys S6; LR=1e-3, Mul-NoRes, Multiple configs; NMF only during training

--- neural_methods/model/FactorizePhys.py
+++ neural_methods/model/FactorizePhys.py
@@ -10,7 +10,7 @@ from torch.nn.modules.batchnorm import _BatchNorm
 import numpy as np
 
 # num_filters
-nf = [6, 12, 12, 12]
+nf = [8, 16, 16, 16]
 
 model_config = {
     "MD_FSAM": True,
@@ -777,19 +777,19 @@ class encoder_block(nn.Module):
         self.debug = debug
 
         self.encoder = nn.Sequential(
-            ConvBlock3D(inCh, nf[1], [3, 3, 3], [1, 1, 1], [1, 0, 0]),
-            ConvBlock3D(nf[1], nf[1], [3, 3, 3], [1, 2, 2], [1, 0, 0]),
-            ConvBlock3D(nf[1], nf[1], [3, 1, 1], [1, 1, 1], [1, 0, 0]),
+            ConvBlock3D(inCh, nf[0], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
+            ConvBlock3D(nf[0], nf[1], [3, 3, 3], [1, 2, 2], [1, 1, 1]),
+            ConvBlock3D(nf[1], nf[1], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
             nn.Dropout3d(p=dropout_rate),
 
-            ConvBlock3D(nf[1], nf[1], [3, 3, 3], [1, 1, 1], [1, 0, 0]),
-            ConvBlock3D(nf[1], nf[2], [3, 3, 3], [1, 2, 2], [1, 0, 0]),
-            ConvBlock3D(nf[2], nf[2], [3, 1, 1], [1, 1, 1], [1, 0, 0]),
+            ConvBlock3D(nf[1], nf[1], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
+            ConvBlock3D(nf[1], nf[2], [3, 3, 3], [1, 2, 2], [1, 1, 1]),
+            ConvBlock3D(nf[2], nf[2], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
             nn.Dropout3d(p=dropout_rate),
 
-            ConvBlock3D(nf[2], nf[2], [3, 3, 3], [1, 1, 1], [1, 0, 0]),
-            ConvBlock3D(nf[2], nf[3], [3, 3, 3], [1, 2, 2], [1, 0, 0]),
-            ConvBlock3D(nf[3], nf[3], [3, 1, 1], [1, 1, 1], [1, 0, 0]),
+            ConvBlock3D(nf[2], nf[2], [3, 3, 3], [1, 1, 1], [1, 1, 1]),
+            ConvBlock3D(nf[2], nf[3], [3, 3, 3], [1, 2, 2], [1, 1, 1]),
+            ConvBlock3D(nf[3], nf[3], [3, 3, 3], [1, 1, 1], [1, 0, 0]),
             nn.Dropout3d(p=dropout_rate)
         )
 
@@ -817,15 +817,21 @@ class BVP_Head(nn.Module):
         else:
             inC = nf[3]
 
-        self.conv_decoder = nn.Sequential(
-            nn.Conv3d(inC, nf[0], (3, 2, 2), stride=(1, 2, 2), padding=(1, 0, 0)),
+        self.conv_decoder1 = nn.Sequential(
+            nn.Conv3d(inC, nf[0]//2, (3, 3, 3), stride=(1, 2, 2), padding=(1, 0, 0)),
             nn.Tanh(),
-            nn.InstanceNorm3d(nf[0]),
-
-            nn.Dropout3d(p=dropout_rate),
-
-            nn.Conv3d(nf[0], 1, (3, 3, 3), stride=(1, 1, 1), padding=(1, 0, 0)),
+            nn.InstanceNorm3d(nf[0]//2),
+            # nn.Dropout3d(p=dropout_rate),
         )
+
+        self.conv_decoder2 = nn.Sequential(
+            nn.Conv3d(inC, nf[0]//2, (5, 5, 5), stride=(1, 1, 1), padding=(2, 0, 0)),
+            nn.Tanh(),
+            nn.InstanceNorm3d(nf[0]//2),
+            # nn.Dropout3d(p=dropout_rate),
+        )
+
+        self.final_layer = nn.Conv3d(nf[0], 1, (3, 3, 3), stride=(1, 1, 1), padding=(1, 0, 0))
 
 
     def forward(self, voxel_embeddings, batch, length):
@@ -861,10 +867,14 @@ class BVP_Head(nn.Module):
             # # Concatenate
             # factorized_embeddings = torch.cat([voxel_embeddings, self.fsam_norm(x)], dim=1)
 
-            x = self.conv_decoder(factorized_embeddings)
+            x1 = self.conv_decoder1(factorized_embeddings)
+            x2 = self.conv_decoder2(factorized_embeddings)
+            x = self.final_layer(torch.cat([x1, x2], dim=1))
         
         else:
-            x = self.conv_decoder(voxel_embeddings)
+            x1 = self.conv_decoder1(voxel_embeddings)
+            x2 = self.conv_decoder2(voxel_embeddings)
+            x = self.final_layer(torch.cat([x1, x2], dim=1))
 
         # x_1d = x.view(batch, -1, length)
 
