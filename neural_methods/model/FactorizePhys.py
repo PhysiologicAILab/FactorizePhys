c487c61087f092941f5029258c5080ed5cc2900a
jnj256 <jnj256@gmail.com>
2024-07-06 10:47:58 +0100

    FactorizePhys Final; LR=1e-3, Mul-NoRes, Multiple configs; NMF only during training

--- neural_methods/model/FactorizePhys.py
+++ neural_methods/model/FactorizePhys.py
@@ -10,7 +10,7 @@ from torch.nn.modules.batchnorm import _BatchNorm
 import numpy as np
 
 # num_filters
-nf = [8, 16, 16, 16]
+nf = [6, 12, 12, 12]
 
 model_config = {
     "MD_FSAM": True,
@@ -760,7 +760,7 @@ class ConvBlock3D(nn.Module):
     def __init__(self, in_channel, out_channel, kernel_size, stride, padding):
         super(ConvBlock3D, self).__init__()
         self.conv_block_3d = nn.Sequential(
-            nn.Conv3d(in_channel, out_channel, kernel_size, stride, padding, bias=False),
+            nn.Conv3d(in_channel, out_channel, kernel_size, stride, padding=padding, bias=True),
             nn.Tanh(),
             nn.InstanceNorm3d(out_channel),
         )
@@ -822,17 +822,13 @@ class BVP_Head(nn.Module):
             inC = nf[3]
 
         self.conv_decoder = nn.Sequential(
-            nn.Conv3d(inC, nf[1], (3, 3, 3), stride=(1, 1, 1), padding=(0, 0, 0), bias=False),
-            nn.Tanh(),
-            nn.InstanceNorm3d(nf[1]),
-
-            nn.Dropout3d(p=dropout_rate),
-
-            nn.Conv3d(nf[1], nf[0], (3, 4, 4), stride=(1, 1, 1), padding=(0, 0, 0), bias=False),
+            nn.Conv3d(inC, nf[0], (3, 4, 4), stride=(1, 2, 2), padding=(0, 0, 0), bias=True),
             nn.Tanh(),
             nn.InstanceNorm3d(nf[0]),
 
-            nn.Conv3d(nf[0], 1, (3, 3, 3), stride=(1, 1, 1), padding=(0, 0, 0), bias=False),
+            nn.Dropout3d(p=dropout_rate),
+
+            nn.Conv3d(nf[0], 1, (3, 3, 3), stride=(1, 1, 1), padding=(0, 0, 0), bias=True),
         )
 
         self.align_length = nn.Upsample((md_config["FRAME_NUM"], 1, 1))
