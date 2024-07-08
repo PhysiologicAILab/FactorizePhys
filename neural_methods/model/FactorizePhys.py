3807449392a4ac858a30b100a3ba0823d2fe730e
jnj256 <jnj256@gmail.com>
2024-07-08 10:14:46 +0100

    FactorizePhys Small - Base and FSAM TrainOnly models

--- neural_methods/model/FactorizePhys.py
+++ neural_methods/model/FactorizePhys.py
@@ -10,10 +10,10 @@ from torch.nn.modules.batchnorm import _BatchNorm
 import numpy as np
 
 # num_filters
-nf = [8, 16, 16, 16]
+nf = [8, 8, 8, 8]
 
 model_config = {
-    "MD_FSAM": False,
+    "MD_FSAM": True,
     "MD_TYPE": "NMF",
     "MD_R": 1,
     "MD_S": 1,
