4404f9ea0bf585dacb45072bfdd1aea9f900eb4e
jnj256 <jnj256@gmail.com>
2024-07-14 12:31:24 +0100

    Finalizing FactorizePhys for Pre-print submission, R=1,S=1,ST=4

--- neural_methods/model/FactorizePhys/FactorizePhys.py
+++ neural_methods/model/FactorizePhys/FactorizePhys.py
@@ -101,9 +101,8 @@ class BVP_Head(nn.Module):
             inC = nf[3]
 
         self.final_layer = nn.Sequential(
-            ConvBlock3D(inC, nf[1], [3, 3, 3], [1, 1, 1], [1, 0, 0]),                          #B, nf[1], 160, 5, 5
-            ConvBlock3D(nf[1], nf[0], [3, 3, 3], [1, 1, 1], [1, 0, 0]),                        #B, nf[0], 160, 3, 3
-            nn.Conv3d(nf[0], 1, (3, 3, 3), stride=(1, 1, 1), padding=(1, 0, 0), bias=False),   #B, 1, 160, 1, 1
+            ConvBlock3D(inC, nf[0], [3, 5, 5], [1, 1, 1], [1, 0, 0]),                          #B, nf[0], 160, 3, 3
+            nn.Conv3d(nf[0], 1, (5, 3, 3), stride=(1, 1, 1), padding=(2, 0, 0), bias=False),   #B, 1, 160, 1, 1
         )
 
     def forward(self, voxel_embeddings, batch, length):
