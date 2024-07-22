0137669878d750e85144613d59c7c497b7049555
jnj256 <jnj256@gmail.com>
2024-07-22 20:17:46 +0100

    Finalizing FSAM for EfficientPhys and FactorizePhys

--- neural_methods/model/FactorizePhys/FactorizePhys.py
+++ neural_methods/model/FactorizePhys/FactorizePhys.py
@@ -102,7 +102,7 @@ class BVP_Head(nn.Module):
             inC = nf[2]
 
         self.final_layer = nn.Sequential(
-            ConvBlock3D(inC, nf[0], [3, 3, 3], [1, 1, 1], [1, 0, 0]),                         #B, nf[0], 160, 3, 3
+            ConvBlock3D(inC, nf[0], [5, 3, 3], [1, 1, 1], [2, 0, 0]),                         #B, nf[0], 160, 3, 3
             nn.Conv3d(nf[0], 1, (3, 3, 3), stride=(1, 1, 1), padding=(1, 0, 0), bias=False),  #B, 1, 160, 1, 1
         )
 
